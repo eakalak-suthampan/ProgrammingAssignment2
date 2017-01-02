feaba05e2dcdb517b01944c12f5503b7489df05e
Eakalak Suthampan <eakalak.j@gmail.com>
2017-01-02 10:09:02 +0700

    1st submit assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Eakalak Suthampan Assignment Week3 for R Programming
 
-## Write a short comment describing this function
-## hello world123
+## creating a matrix that can cache inverse. 
+
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inv) m <<- inv
+        getinv <- function() m
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## checking whether or not the inverse was cached. if not, then find inverse using solve(). 
+## Usage Example: z <- makeCacheMatrix(matrix(1:4,2,2))
+##                cacheSolve(z)
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinv(m)
+        m
 }
